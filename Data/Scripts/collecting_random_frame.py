2a3daffee47bf2af4b38df33fa2f69b49f1fb0ea
Kylian Santos <kyliansts@gmail.com>
2025-11-03 16:25:52 +0100

    download and extrac frame to label

---
+++ Data/Scripts/collecting_random_frame.py
@@ -0,0 +1,74 @@
+import yt_dlp
+import cv2
+import random
+import os
+
+def download_youtube_videos(urls: list, output_dir: str = "videos") -> list:
+    """
+    Download multiple YouTube videos given a list of URLs.
+    Returns a list of downloaded video file paths.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    downloaded_paths = []
+    for url in urls:
+        ydl_opts = {
+            'outtmpl': os.path.join(output_dir, '%(title)s.%(ext)s'),
+            'format': 'bestvideo+bestaudio/best',
+            'quiet': True,
+            'merge_output_format': 'mp4',
+            'cookiesfrombrowser': ['firefox'],
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+            },
+         }
+        
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+            if not filename.endswith(".mp4"):
+                filename = os.path.splitext(filename)[0] + ".mp4"
+            downloaded_paths.append(filename)
+
+    return downloaded_paths
+
+
+def extract_random_frames(video_paths: list, output_dir: str, num_frames: int):
+    """
+    Extract `num_frames` random frames from each video in `video_paths`
+    and save them directly into `output_dir.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+
+    for video_path in video_paths:
+        cap = cv2.VideoCapture(video_path)
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        video_name = os.path.splitext(os.path.basename(video_path))[0]
+
+        for i in range(num_frames):
+            frame_id = random.randint(0, total_frames - 1)
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+            success, frame = cap.read()
+            if success:
+                filename = os.path.join(output_dir, f"{video_name}_frame_{i+1}.jpg")
+                cv2.imwrite(filename, frame)
+
+        cap.release()
+
+
+if __name__ == "__main__":
+    
+    # Official highligts of the seasons 2021 to 2023 that last arround 40 minutes each
+    taining_urls = [
+        "https://www.youtube.com/watch?v=-zcIsk6GE6Q",
+        "https://www.youtube.com/watch?v=dxbVdyVYGcQ",
+        "https://www.youtube.com/watch?v=hFrIVlkTDMs",
+    ]
+
+    test_url = ["https://www.youtube.com/watch?v=_e2tSzsuark"]
+
+    train_videos_path = "Data/train_videos"
+    file_paths = [os.path.join(train_videos_path, f) for f in os.listdir(train_videos_path) if os.path.isfile(os.path.join(train_videos_path, f))]
+    
+    extract_random_frames(file_paths, "Data/train_images", 500)
+    #extract_random_frames(b, "Data/test_images", 100)
